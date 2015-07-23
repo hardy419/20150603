50c29765b26e62b6ab3a5b0a8d20afcb8d8c9054
Hardy <540115739@qq.com>
2015-07-23 10:55:12 +0800

    Fixes 7-23.

--- ex_replace.php
+++ ex_replace.php
@@ -3,7 +3,7 @@ header("Content-type: text/html; charset=utf-8");
 
 $files=scandir('.');
 
-foreach($files as $file) if('artwork_m' == substr($file,0,9)) {
+foreach($files as $file) if('artwork_' == substr($file,0,8)) {
     //echo "<h1>$file</h1>";
     $html=file_get_contents($file);
 
@@ -21,8 +21,8 @@ foreach($files as $file) if('artwork_m' == substr($file,0,9)) {
     array_push($str2, '1.4D 有向數四則計算');
     array_push($str1, '角平分線');
     array_push($str2, '有向數');*/
-    array_push($str1, '</table>');
-    array_push($str2, '<tr><td><p class="chi">類別/Category</p><p class="chi">&nbsp;</p></td><td><p class="chi">媒體藝術/Media Art</p><p class="chi">&nbsp;</p></td></tr></table>');
+    array_push($str1, '<p class="chi">中國書畫/Chinese Painting &amp; Calligraphy</p>');
+    array_push($str2, '<p class="chi">中國書畫</p><p class="eng">Chinese Painting &amp; Calligraphy</p>');
     //////////////////////////////////////////////////////////////
 
     $f_con=str_replace($str1,$str2,$html);
